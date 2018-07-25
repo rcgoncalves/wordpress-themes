11ac9d0b441ee6b439919229fae6eda944e8539d
rcg <rcgoncalves.pt@gmail.com>
2018-07-25 17:34:53 +0100

    Minor improvements

--- footer.php
+++ footer.php
@@ -7,9 +7,9 @@
 			?>
 			<footer id="colophon" role="contentinfo">
 				<div class="site-info">
-					<a href="http://wordpress.org/" title="<?php esc_attr_e('Semantic Personal Publishing Platform', 'rcg-gray'); ?>"><?php _e('Proudly powered by WordPress', 'rcg-gray' ); ?></a>
+					<a href="https://wordpress.org/" title="<?php esc_attr_e('Semantic Personal Publishing Platform', 'rcg-gray'); ?>"><?php _e('Proudly powered by WordPress', 'rcg-gray' ); ?></a>
 					|
-					<a href="http://rcgoncalves.pt/project/rcg-gray/"><?php _e('Theme RCG Gray', 'rcg-gray'); ?></a>
+					<a href="https://rcgoncalves.pt/project/rcg-gray/"><?php _e('Theme RCG Gray', 'rcg-gray'); ?></a>
 				</div>
 			</footer>
 		</div>
